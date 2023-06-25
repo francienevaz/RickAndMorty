abb9b3d90723d202b8d614f5ab450fd187ce60f2
francienevaz <fran.vfraga@gmail.com>
2023-06-25 14:22:53 -0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import RickAndMortyCard from '../components/Card'
+import { Container } from '@mui/system'
 
 export const Home = () => {
     return (
         <div>
             <NavBar/>
+            <Container maxWidth="false">
+               <RickAndMortyCard /> 
+            </Container>
+            
         </div>
     )
 }
