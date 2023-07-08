84efddd4125812ca372bba3200111d43ed02cd93
francienevaz <fran.vfraga@gmail.com>
2023-07-08 13:11:32 -0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
 
     const getCharacters = () => {
         let endpoints = [];
-        for (let i = 1; i < 105; i++) {
+        for (let i = 1; i < 150; i++) {
             endpoints.push(`https://rickandmortyapi.com/api/character/${i}/`)
         }
         axios.all(endpoints.map((endpoint) => axios
